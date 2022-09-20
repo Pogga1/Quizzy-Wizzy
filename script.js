e0051d6c28122e071c37cb5efb3944aeff39e119
Pogga1 <Andreweysoldt@gmail.com>
2022-09-19 19:01:44 -0700

    added needed javascript variables and created baswork for functions

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
 let beginButton = document.getElementById("startButt");
-let timer = document.getElementById("timer")
+let score=0;
+let questionIndex=0;
+let timeStart=60;
+let article1= document.getElementById("information");
+let questionsContainer= document.getElementById("questionsBox");
+let createUl= document.createElement("ul");
+let startButt = document.getElementById("startButt");
+let timeLeft=document.getElementById("timeLeft");
 let timeSub= 3;
 const multiQuestions = [{
         Question: "Where is the data in localStorage stored?",
-        Choices: "A. The cloud", "B.User's Browser", "C. Your Local Drive", "D. A random company's server",
+        Choices: ["A. The cloud", "B.User's Browser", "C. Your Local Drive", "D. A random company's server"],
         Answer: "B.User's Browser"
     },
     {
         Question: "What do we need to do to stop event bubbling?",
-        Choices: "A. use Document.getElementbyId", "B. use event.stopPropogate()", "C. use Document.appendChild", "D. Use event.stopPropogation",
+        Choices: ["A. use Document.getElementbyId", "B. use event.stopPropogate()", "C. use Document.appendChild", "D. Use event.stopPropogation"],
         Answer: "D. Use event.stopPropogation()"
     },
     {
-        Question: "Which two parameters does the addEventListener() method need to execute?”,
-        Choices: "A. name of event, action", "B. Number, id", "C. function, action", "D. name of event, #class", 
+        Question: "Which two parameters does the addEventListener() method need to execute?",
+        Choices: ["A. name of event, action", "B. Number, id", "C. function, action", "D. name of event, #class"], 
         Answer:"A. name of event, action"
     },
     {
-        Question: "How do we create a function in JavaScript?”,
-        Choices:"A. function a()","B. function= a()","C. declareFunction a()", "D, fun a()"
+        Question: "How do we create a function in JavaScript?",
+        Choices:["A. function a()","B. function= a()","C. declareFunction a()", "D, fun a()"],
         Answer:"A. function a()"
     },
     {
-        Question: "How can we use an element's id to access that element?”,
-        Choices: "A. getElementbyclass()","B. getElementId()","C. getElementbyid()","D.getid()",
+        Question: "How can we use an element's id to access that element?",
+        Choices: ["A. getElementbyclass()","B. getElementId()","C. getElementbyid()","D.getid()"],
         Answer:"C. getElementbyid()"
     }
 ];
 
-function jump() {
+startButt.addEventListener("click",function());
+
+
+
+function jumpIn() {
 
 }
 
@@ -39,7 +50,8 @@ function jump() {
 
 
 
-startButt.addEventListener("click",/*function()*/)
+
+
 
 
 //click Jump in button to start quiz
